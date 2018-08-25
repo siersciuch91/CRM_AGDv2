497004951907d1afe5156438c3d2c8aa2009b991
Adrian Grzegorek <siersciuch1991@gmail.com>
2018-08-25 10:44:46 +0200

    Add project files.

---
+++ CRM_AGD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRM_AGD.Startup))]
+namespace CRM_AGD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
